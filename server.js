eb02fb23ecb9630231ff218be3227ae71cd4dd5b
bbrad3 <68521389+bbrad3@users.noreply.github.com>
2021-05-21 09:56:12 -0600

    Setup express server and sequelize inited

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+
+const routesReport = require('rowdy-logger').begin(app)
+
+// MIDDLEWARE
+app.use(express.json())
+app.use(require('morgan')('dev'))
+app.use(require('cors')())
+require('dotenv').config()
+
+// ROUTES
+const userRouter = require('./routers/userRouter')
+app.use('/user', userRouter)
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+    routesReport.print()
+})
